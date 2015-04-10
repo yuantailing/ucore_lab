9cf197b5f75e1704361c7dde7da3c48e8f5c074b
yuantailing <yuantailing@126.com>
2015-04-10 21:24:17 +0800

    lab3.ex0

--- labcodes/lab3/kern/mm/default_pmm.c
+++ labcodes/lab3/kern/mm/default_pmm.c
@@ -65,21 +65,6 @@ default_init(void) {
     nr_free = 0;
 }
 
-static void
-default_init_memmap(struct Page *base, size_t n) {
-    assert(n > 0);
-    struct Page *p = base;
-    for (; p != base + n; p ++) {
-        assert(PageReserved(p));
-        p->flags = p->property = 0;
-        set_page_ref(p, 0);
-    }
-    base->property = n;
-    SetPageProperty(base);
-    nr_free += n;
-    list_add(&free_list, &(base->page_link));
-}
-
 static struct Page *
 default_alloc_pages(size_t n) {
     assert(n > 0);
@@ -96,14 +81,21 @@ default_alloc_pages(size_t n) {
         }
     }
     if (page != NULL) {
-        list_del(&(page->page_link));
+        int i;
+        list_entry_t *le_to_change = &page->page_link;
+        for (i = 0; i < n; i++) {
+              struct Page *p = le2page(le_to_change, page_link);
+              ClearPageProperty(p);
+              SetPageReserved(p);
+              le_to_change = list_next(le_to_change);
+        }
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
-            list_add(&free_list, &(p->page_link));
-    }
+            list_add(&page->page_link, &(p->page_link));
+        }
+        list_del(&(page->page_link));
         nr_free -= n;
-        ClearPageProperty(page);
     }
     return page;
 }
@@ -113,30 +105,29 @@ default_free_pages(struct Page *base, size_t n) {
     assert(n > 0);
     struct Page *p = base;
     for (; p != base + n; p ++) {
-        assert(!PageReserved(p) && !PageProperty(p));
-        p->flags = 0;
+        p->flags = p->property = 0;
+        SetPageProperty(p);
         set_page_ref(p, 0);
     }
     base->property = n;
-    SetPageProperty(base);
     list_entry_t *le = list_next(&free_list);
+    for (; le != &free_list; le = list_next(le))
+        if (le2page(le, page_link) > base)
+            break;
+    list_add_before(le, &base->page_link);
+    le = list_next(&free_list);
     while (le != &free_list) {
-        p = le2page(le, page_link);
-        le = list_next(le);
-        if (base + base->property == p) {
-            base->property += p->property;
-            ClearPageProperty(p);
-            list_del(&(p->page_link));
-        }
-        else if (p + p->property == base) {
-            p->property += base->property;
-            ClearPageProperty(base);
-            base = p;
-            list_del(&(p->page_link));
-        }
+        list_entry_t *le_next = list_next(le);
+        struct Page *p = le2page(le, page_link);
+        struct Page *p_next = le2page(le_next, page_link);
+        if (p + p->property == p_next) {
+            p->property += p_next->property;
+            p_next->property = 0;
+            list_del(&(p_next->page_link));
+        } else
+            le = le_next;
     }
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
 }
 
 static size_t
@@ -263,7 +254,7 @@ default_check(void) {
 const struct pmm_manager default_pmm_manager = {
     .name = "default_pmm_manager",
     .init = default_init,
-    .init_memmap = default_init_memmap,
+    .init_memmap = default_free_pages,
     .alloc_pages = default_alloc_pages,
     .free_pages = default_free_pages,
     .nr_free_pages = default_nr_free_pages,
